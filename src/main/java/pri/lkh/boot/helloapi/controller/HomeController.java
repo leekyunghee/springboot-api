2f18413027ba71861d4531b6b93dc9c3815a4a81
leekyunghee <qkqhlkh@gmail.com>
2019-07-25 17:49:35 +0900

    Datasource setting and Create HomeController

---
+++ src/main/java/pri/lkh/boot/helloapi/controller/HomeController.java
@@ -0,0 +1,17 @@
+package pri.lkh.boot.helloapi.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class HomeController {
+
+    @GetMapping("/greet")
+    @ResponseBody
+    public String hello() {
+        return "hello world";
+
+    }
+}
---
+++ src/test/java/pri/lkh/boot/helloapi/HelloBootApplicationTests.java
@@ -0,0 +1,18 @@
+package pri.lkh.boot.helloapi;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class HelloBootApplicationTests {
+
+    @Test
+    public void contextLoads() {
+
+    }
+}
+
+
